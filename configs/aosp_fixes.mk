2620a94be1aa54b475eed99ab29306d2429b10fe
viperusgit <viperusgit@gmail.com>
2017-08-30 19:56:21 -0500

    Add in dialer_experience.xml to fix google phone bitching at us

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -40,3 +40,7 @@ PRODUCT_COPY_FILES +=  \
     vendor/nexus/prebuilt/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     vendor/nexus/prebuilt/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
 endif
+
+# Fix Dialer
+PRODUCT_COPY_FILES +=  \
+    vendor/nexus/prebuilt/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml 
