5949d4506c2b4b8755d7474aca6836f5129ee79c
ezio84 <brabus84@gmail.com>
2018-04-03 11:25:27 +0200

    Update prebuilt apk from April Marlin img

--- prebuilt/app/Android.mk
+++ prebuilt/app/Android.mk
@@ -15,19 +15,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := WallpaperPickerGooglePrebuilt
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := WallpaperPickerGooglePrebuilt/WallpaperPickerGooglePrebuilt.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_OVERRIDES_PACKAGES := \
-    WallpaperPicker \
-    WallpaperCropper
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := PrebuiltDeskClockGoogle
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := PrebuiltDeskClockGoogle/PrebuiltDeskClockGoogle.apk
--- prebuilt/priv-app/Android.mk
+++ prebuilt/priv-app/Android.mk
@@ -15,6 +15,20 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := WallpaperPickerGooglePrebuilt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := WallpaperPickerGooglePrebuilt/WallpaperPickerGooglePrebuilt.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_OVERRIDES_PACKAGES := \
+    WallpaperPicker \
+    WallpaperCropper
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := Turbo
 LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
